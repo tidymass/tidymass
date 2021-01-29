7962982c936752c99cae0b44e584e354f639e32f
jaspershen <shenxt@DNab421cd5.stanford.edu>
2021-01-28 23:10:04 -0800

    first commit

---
+++ R/logo.R
@@ -0,0 +1,42 @@
+#' The tidymass logo, using ASCII or Unicode characters
+#'
+#' Use [crayon::strip_style()] to get rid of the colors.
+#'
+#' @param unicode Whether to use Unicode symbols. Default is `TRUE`
+#'   on UTF-8 platforms.
+#'
+#' @md
+#' @export
+#' @examples
+#' tidymass_logo()
+
+tidymass_logo <- function(unicode = l10n_info()$`UTF-8`) {
+  logo <- c(
+    "0 __  _    __   1    2           3  4 ",
+    " / /_(_)__/ /_ ___  _____ _______ ___ ",
+    "/ __/ / _  / // / |/ / -_) __(_-</ -_)",
+    "\\__/_/\\_,_/\\_, /|___/\\__/_/ /___/\\__/ ",
+    "     5  6 /___/      7      8       9 ")
+  
+  hexa <- c("*", ".", "o", "*", ".", "*", ".", "o", ".", "*")
+  if (unicode) hexa <- c("*" = "\u2b22", "o" = "\u2b21", "." = ".")[hexa]
+  
+  cols <- c("red", "yellow", "green", "magenta", "cyan",
+            "yellow", "green", "white", "magenta", "cyan")
+  
+  col_hexa <- purrr::map2(hexa, cols, ~ crayon::make_style(.y)(.x))
+  
+  for (i in 0:9) {
+    pat <- paste0("\\b", i, "\\b")
+    logo <- sub(pat, col_hexa[[i + 1]], logo)
+  }
+  
+  structure(crayon::blue(logo), class = "tidymass_logo")
+}
+
+#' @export
+
+print.tidymass_logo <- function(x, ...) {
+  cat(x, ..., sep = "\n")
+  invisible(x)
+}
